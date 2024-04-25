1d4b52c6f7e32829e1d62612f1840eb04128a288
unknown <rlvelazquez86@gmail.com>
2024-04-25 09:43:06 -0500

    annadiendo mi modulo

--- real__estate/__manifest__.py
+++ real__estate/__manifest__.py
@@ -24,9 +24,13 @@
 
     # always loaded
     "data": [
-        
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml'
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_property_type_views.xml",
+        "views/estate_property_tag_views.xml",
+        "views/estate_menu.xml",
+        "views/estate_property_offer_views.xml",
+        "security/ir.model.access.csv"
     ],
     'installable': True,
     'application': True,
